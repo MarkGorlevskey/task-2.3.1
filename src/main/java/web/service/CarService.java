9291856909569c1843cf7f780bc3201a5653f6c6
MarkGorlevskey <mgorlevskey@yandex.ru>
2023-04-15 14:51:01 +0300

    #1

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,11 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.List;
+
+public interface CarService {
+    void addCar(Car car);
+    List<Car> getCar(Integer count);
+    void setCars(List<Car> cars);
+}
